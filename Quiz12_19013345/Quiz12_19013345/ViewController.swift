03846774e4e51473197f03fb769c0295fdf94450
Ju Long <19013345@myrp.edu.sg>
2021-02-04 15:43:41 +0800

    commit

---
+++ Quiz12_19013345/Quiz12_19013345/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Quiz12_19013345
+//
+//  Created by day4 on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var outTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
+    
+    func tableView(_ )
+}
+
